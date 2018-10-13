b23fa1cd9f3522a41921e08ba6c64eed6f93a433
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2018-10-13 16:53:43 +0330

    Add soap client

---
+++ client.js
@@ -0,0 +1,20 @@
+'use strict';
+const soap = require('soap');
+let url = 'http://localhost:3001/wsdl?wsdl';
+
+async function behx() {
+  try {
+  let client = await soap.createClientAsync(url);
+
+  let args = {
+    message: "id1:12:34:56:out42",
+    splitter: ":"
+  };
+  let result = await client.MessageSplitterAsync(args);
+  console.log(result[0]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+behx();
